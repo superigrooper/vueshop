b62d011454fb9963d96cffc650830e75ca7f14bd
vitalii_terentev_suchkov <tevvit@gmail.com>
2022-04-04 18:37:42 +0300

    Добавленны тестовые файлы data.js, run.js

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue';
 import App from './App.vue';
+import { vue, js } from './data';
+import print from './run';
 
 Vue.config.productionTip = false;
 
 new Vue({
   render: (h) => h(App),
 }).$mount('#app');
+
+print(vue);
+print(js);
